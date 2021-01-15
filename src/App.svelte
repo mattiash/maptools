f8ca76c9c580d23b8a21de3aec9ce3cd89e60249
Mattias Holmlund <mattias@holmlund.se>
2021-01-15 19:24:48 +0100

    Sample app with leafletjs

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,71 @@
-<script lang="ts">
-	export let name: string;
+<script>
+  import {
+    CircleMarker,
+    LeafletMap,
+    Marker,
+    Polygon,
+    Popup,
+    TileLayer,
+    Tooltip,
+  } from "svelte-leafletjs";
+  import TestData from "./TestData";
+
+  const mapOptions = {
+    center: [1.364917, 103.822872],
+    zoom: 11,
+  };
+  const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+  const tileLayerOptions = {
+    minZoom: 0,
+    maxZoom: 20,
+    maxNativeZoom: 19,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  };
+
+  let leafletMap;
+
+  function mousemove(ev) {
+    for (const p of TestData.points) {
+      p[3] = p[0] < ev.detail.latlng.lat;
+    }
+    TestData.points = TestData.points;
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="example">
+  <LeafletMap
+    bind:this={leafletMap}
+    options={mapOptions}
+    events={["mousemove"]}
+    on:mousemove={mousemove}
+  >
+    <TileLayer url={tileUrl} options={tileLayerOptions} />
+    <Polygon
+      latLngs={TestData.sentosaPolygon}
+      color="#ff0000"
+      fillColor="#ff0000"
+    >
+      <Popup>Sentosa</Popup>
+      <Tooltip>Sentosa</Tooltip>
+    </Polygon>
+    {#each TestData.points as point}
+      <CircleMarker
+        latLng={[point[0], point[1]]}
+        color={point[3] ? "#ff0000" : "#0000ff"}
+      >
+        <Tooltip>{point[2]}</Tooltip>
+      </CircleMarker>
+    {/each}
+  </LeafletMap>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  /* NOTE: Typically not imported from here, see documentation for more information. */
+  @import "https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.css";
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .example {
+    width: 100%;
+    height: 100%;
+  }
 </style>
